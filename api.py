04f0306e36a0b775753c94aaf096c399e58a3dbc
John Laine <john.laine5896@gmail.com>
2016-10-28 18:48:01 +0000

    Issue #4: Add basic functionality to create a game
    Add ability to create a game a store in the database.

--- api.py
+++ api.py
@@ -3,12 +3,13 @@ from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
 
-from models import User
-from models import Test, StringMessage
+from models import User, Game
+from models import Test, StringMessage, CreateGameForm, GameState
 
 ##### RESOURCE CONTAINERS #####
 CREATE_USER_REQUEST = endpoints.ResourceContainer(
                       user_name = messages.StringField(1, required = True))
+CREATE_GAME_REQUEST = endpoints.ResourceContainer(CreateGameForm)
     
 ##### GAME API #####
 @endpoints.api(name='hangman', version = 'v1')
@@ -39,5 +40,20 @@ class HangmanAPI(remote.Service):
         user.put()
         return StringMessage(message = 'User {} has been created'.format(
             request.user_name))
-        
+
+
+    @endpoints.method(request_message=CreateGameForm,
+                      response_message=GameState,
+                      path='game',
+                      name='create_game',
+                      http_method='POST')
+    def create_game(self, request):
+        """Create a game"""
+        user = User.query(User.name == request.user_name).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                'A user with that name does not exist!')
+        game = Game.create_game(user.key, request.attempts_allowed)
+        return game.game_state('A new game of Hangman was created!')
+                    
 api = endpoints.api_server([HangmanAPI])
--- models.py
+++ models.py
@@ -5,10 +5,32 @@ from google.appengine.ext import ndb
 ##### DATABASE MODELS #####
 
 class User(ndb.Model):
-    """A User Profile"""
-    name = ndb.StringProperty(required = True)
+    """A User Profile object"""
+    name = ndb.StringProperty(required=True)
 
-
+class Game(ndb.Model):
+    """A Game object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    attempts_allowed = ndb.IntegerProperty(required=True)
+    attempts_remaining = ndb.IntegerProperty(required=True, default=5)
+    
+    @classmethod
+    def create_game(cls, user, attempts_allowed):
+        """Creates and returns a new game"""
+        game = Game(user=user,
+                    attempts_allowed=attempts_allowed)
+        game.put()
+        return game
+        
+    def game_state(self, message):
+        """Returns the state of a game"""
+        state = GameState()
+        state.urlsafe_game_key = self.key.urlsafe()
+        state.user_name = self.user.get().name
+        state.attempts_remaining = self.attempts_remaining
+        state.message = message
+        return state
+        
 ##### MESSAGES #####
 
 class Test(messages.Message):
@@ -17,4 +39,18 @@ class Test(messages.Message):
     
 class StringMessage(messages.Message):
     """A generic outbound string message"""
-    message = messages.StringField(1, required = True)
+    message = messages.StringField(1, required=True)
+    
+class CreateGameForm(messages.Message):
+    """Inbound, used to create a new game"""
+    user_name = messages.StringField(1, required=True)
+    attempts_allowed = messages.IntegerField(2, default=5)
+    
+class GameState(messages.Message):
+    """Outbound game state information"""
+    urlsafe_game_key = messages.StringField(1, required=True)
+    user_name = messages.StringField(2, required=True)
+    attempts_remaining = messages.IntegerField(3, required=True)
+    message = messages.StringField(4, required=True)
+    
+    
