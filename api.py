befc99c5368f84bb4877bee06e76ac7d129a9f86
John Laine <john.laine5896@gmail.com>
2016-10-28 17:23:00 +0000

    Issue #3: Add ability to add user to database

--- api.py
+++ api.py
@@ -3,8 +3,14 @@ from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
 
-from models import Test
+from models import User
+from models import Test, StringMessage
 
+##### RESOURCE CONTAINERS #####
+CREATE_USER_REQUEST = endpoints.ResourceContainer(
+                      user_name = messages.StringField(1, required = True))
+    
+##### GAME API #####
 @endpoints.api(name='hangman', version = 'v1')
 class HangmanAPI(remote.Service):
     """Hangman Game API"""
@@ -17,4 +23,21 @@ class HangmanAPI(remote.Service):
         return Test(message = 'This is a test, this is only a test')
         
     
+    @endpoints.method(request_message = CREATE_USER_REQUEST,
+                      response_message = StringMessage,
+                      path = 'user',
+                      name = 'create_user',
+                      http_method = 'POST')
+    def create_user(self, request):
+        """Create a user"""
+        # Check if the user name already exists
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                'Sorry, that username already exists')
+        # Create the user and store in the database
+        user = User(name = request.user_name)
+        user.put()
+        return StringMessage(message = 'User {} has been created'.format(
+            request.user_name))
+        
 api = endpoints.api_server([HangmanAPI])
--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 from protorpc import messages
 from google.appengine.ext import ndb
 
+
+##### DATABASE MODELS #####
+
+class User(ndb.Model):
+    """A User Profile"""
+    name = ndb.StringProperty(required = True)
+
+
+##### MESSAGES #####
+
 class Test(messages.Message):
     """This is a test endpoint, for dev purposes"""
     message = messages.StringField(1)
     
-class User(ndb.Model):
-    pass
-
-class Game(ndb.Model):
-    pass
+class StringMessage(messages.Message):
+    """A generic outbound string message"""
+    message = messages.StringField(1, required = True)
