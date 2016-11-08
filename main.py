c6a39bcebe951a1e9ce3aa36da196e909fec5b0f
John Laine <john.laine5896@gmail.com>
2016-11-08 18:27:54 +0000

    Remove development testing code

--- main.py
+++ main.py
@@ -13,10 +13,6 @@ from models import User, Game
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
-        pass
-
-class SendTestEmail(webapp2.RequestHandler):
-    def get(self):
         """Send a reminder email to users.
 
         This handler will be called every hour using a cron job.
@@ -38,11 +34,11 @@ class SendTestEmail(webapp2.RequestHandler):
                 email_to = user.email
                 email_subject = 'Have you given up on your game of Hangman?'
                 email_body = ('Hello {}, it\'s been awhile since you played'
-                              ' your game of hangman').format(user.user_name)
+                              ' your game of hangman. why don\'t you come back'
+                              ' and play for awhile').format(user.user_name)
                 mail.send_mail(email_from, email_to, email_subject, email_body)
 
 
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendReminderEmail),
-    ('/sendtestemail', SendTestEmail),
 ], debug=True)
